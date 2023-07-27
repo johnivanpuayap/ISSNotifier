513512d98bf8637fff92ff8033a592369a33cef3
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-27 21:34:08 +0800

    Add Night Checker and ISS Overhead Checker

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def is_iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    # # Your position is within +5 or -5 degrees of the ISS position.
-    # if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
-    #     return True
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
     return True
 
 
@@ -38,8 +38,8 @@ def is_night():
 
     hour_now = datetime.now().hour
 
-    # if hour_now <= sunrise or hour_now >= sunrise:
-    #     return True
+    if hour_now <= sunrise or hour_now >= sunrise:
+        return True
     return True
 
 
